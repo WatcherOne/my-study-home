2afea3a58d368e116c89eb14a01c4e620eedaafb
zhubo <286154864@qq.com>
2023-05-11 14:28:48 +0800

    add some database

--- book_house.sql
+++ book_house.sql
@@ -11,11 +11,27 @@
  Target Server Version : 80032
  File Encoding         : 65001
 
- Date: 11/05/2023 13:44:18
+ Date: 11/05/2023 14:28:03
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for author
+-- ----------------------------
+DROP TABLE IF EXISTS `author`;
+CREATE TABLE `author`  (
+  `id` int(0) NOT NULL COMMENT '主键',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '作者姓名',
+  `nation` int(0) DEFAULT NULL COMMENT '作者国籍（关联字典Id）',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '作家表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of author
+-- ----------------------------
+INSERT INTO `author` VALUES (1, '中岛敦', 2);
 
 -- ----------------------------
 -- Table structure for book
@@ -24,12 +40,72 @@ DROP TABLE IF EXISTS `book`;
 CREATE TABLE `book`  (
   `id` int(0) NOT NULL COMMENT '主键',
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '书名',
-  `author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '作者',
+  `author_id` int(0) DEFAULT NULL COMMENT '关联作者Id',
+  `cover` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '封面',
   `tags` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '标签（关联ids）',
   `introduction` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '简介',
   `publish_time` datetime(0) DEFAULT NULL COMMENT '出版时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '书籍表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of book
+-- ----------------------------
+INSERT INTO `book` VALUES (1, '山月记', 1, NULL, NULL, NULL, NULL);
+
+-- ----------------------------
+-- Table structure for book_user
+-- ----------------------------
+DROP TABLE IF EXISTS `book_user`;
+CREATE TABLE `book_user`  (
+  `user_id` int(0) NOT NULL COMMENT '用户Id',
+  `book_id` int(0) NOT NULL COMMENT '书籍Id',
+  `is_read` int(0) DEFAULT 0 COMMENT '读状态（0：未读，1：在读，2：已读）',
+  `create_time` datetime(0) DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`user_id`, `book_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '书籍与用户状态记录表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for dict_type
+-- ----------------------------
+DROP TABLE IF EXISTS `dict_type`;
+CREATE TABLE `dict_type`  (
+  `id` int(0) NOT NULL COMMENT '主键',
+  `dict_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '字典名称',
+  `dict_code` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '字典编码（用于标识哪种类型的字典）',
+  `create_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '创建人',
+  `create_time` datetime(0) DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '更新人',
+  `update_time` datetime(0) DEFAULT NULL COMMENT '更新时间',
+  `remarks` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '字典类型表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of dict_type
+-- ----------------------------
+INSERT INTO `dict_type` VALUES (1, '国籍', 'nation', NULL, NULL, NULL, NULL, '作者国籍');
+
+-- ----------------------------
+-- Table structure for dict_type_data
+-- ----------------------------
+DROP TABLE IF EXISTS `dict_type_data`;
+CREATE TABLE `dict_type_data`  (
+  `id` int(0) NOT NULL COMMENT '主键Id',
+  `dict_type` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '字典类型（关联的字段编码）',
+  `dict_label` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '字典标签',
+  `dict_value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '字典键值',
+  `dict_sort` int(0) DEFAULT NULL COMMENT '字典排序',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '字典数据表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of dict_type_data
+-- ----------------------------
+INSERT INTO `dict_type_data` VALUES (1, 'nation', '中国', '1', 1);
+INSERT INTO `dict_type_data` VALUES (2, 'nation', '日本', '2', 2);
+INSERT INTO `dict_type_data` VALUES (3, 'nation', '德国', '3', 3);
+INSERT INTO `dict_type_data` VALUES (4, 'nation', '法国', '4', 4);
 
 -- ----------------------------
 -- Table structure for tag
@@ -60,17 +136,6 @@ CREATE TABLE `user`  (
   `update_time` datetime(0) DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for user_book
--- ----------------------------
-DROP TABLE IF EXISTS `user_book`;
-CREATE TABLE `user_book`  (
-  `user_id` int(0) NOT NULL COMMENT '用户Id',
-  `book_id` int(0) NOT NULL COMMENT '书籍Id',
-  `create_time` datetime(0) DEFAULT NULL COMMENT '创建时间',
-  PRIMARY KEY (`user_id`, `book_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户关联书籍表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user_book_list
