e9a8ce0b2874ef924d7bd881946ddf2a12ca6877
watcher <286154864@qq.com>
2023-05-25 20:12:16 +0800

    delete more file

--- src/pages/bookmark/[id].tsx
+++
@@ -1,8 +0,0 @@
-// 收藏夹详细
-import { useRouter } from 'next/router'
-
-export default function Page () {
-    const router = useRouter()
-
-    return <div>Post: { router.query.id }</div>
-}
--- src/pages/personal/myFavorites.tsx
+++
@@ -1,5 +0,0 @@
-// 我的收藏夹
-
-export default function MyFavorites () {
-    return <div>About</div>
-}
