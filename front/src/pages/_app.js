ca118041725f6d72d615d5412f9ba18b5a5f3c10
zhubo <286154864@qq.com>
2023-05-30 13:08:10 +0800

    add some reportWebVitals console

--- front/src/pages/_app.js
+++ front/src/pages/_app.js
@@ -1,6 +1,35 @@
 // 导入全局样式
 import '@/styles/globals.css'
 
+export function reportWebVitals (metric) {
+    console.log(metric)
+}
+
+// export function reportWebVitals(metric) {
+//     switch (metric.name) {
+//       case 'FCP':
+//         // handle FCP results
+//         break;
+//       case 'LCP':
+//         // handle LCP results
+//         break;
+//       case 'CLS':
+//         // handle CLS results
+//         break;
+//       case 'FID':
+//         // handle FID results
+//         break;
+//       case 'TTFB':
+//         // handle TTFB results
+//         break;
+//       case 'INP':
+//         // handle INP results (note: INP is still an experimental metric)
+//         break;
+//       default:
+//         break;
+//     }
+//   }
+
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
 }
