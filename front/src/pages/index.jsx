4cbf9dcb22bd76ad13e8c74a9af5b62948ba8edd
zhubo <286154864@qq.com>
2023-06-01 16:55:00 +0800

    fxied api position

--- front/src/pages/index.jsx
+++ front/src/pages/index.jsx
@@ -5,6 +5,7 @@ export default function Home() {
     return (
         <section className={styles.container}>
             <div className="title">首页</div>
+            <Link href="/my/bookGroup">我的书单</Link>
         </section>
     )
 }
